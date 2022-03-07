6b212ab5216858b06a5e8227948c17e63bc54ef7
Wadehl <100260428+Wadehl@users.noreply.github.com>
2022-03-07 09:13:09 +0800

    Update and rename mycnt2.js to mycnt3.js

---
+++ MyBlogdata/js/mycnt3.js
@@ -0,0 +1,33 @@
+'use strict';
+//timeCounter
+    var div4 = document.getElementsByTagName("cnt")[0];
+ 
+        var timer1 = setInterval(fn, 1);//设置定时器，1毫秒跳动1次
+ 
+        function fn() {
+            //获取时间差：未来时间减去此刻的毫秒值
+            var nowtime1 = new Date();
+            var former = new Date("2022/03/03 12:00:00");
+            var timeSum1 = nowtime1.getTime() - former.getTime();
+ 
+            //根据时间差分别计算天数，小时数等，parseInt() 函数可解析一个字符串，并返回一个整数。
+ 
+            var day1 = parseInt(timeSum1 / 1000 / 60 / 60 / 24);
+            var hour1 = parseInt(timeSum1 / 1000 / 60 / 60 % 24);
+            var minu1 = parseInt(timeSum1 / 1000 / 60 % 60);
+            var sec1 = parseInt(timeSum1 / 1000 % 60);
+            var millsec1 = parseInt(timeSum1 % 1000);
+ 
+            //问题处理：所有的时间小于10的时候，在前面自动补0，毫秒值要补双0（比如如，把 8 秒改成 08 秒）
+            day1 = day1 < 10 ? "0" + day1 : day1;  //day小于10吗？如果小于，就补0；如果不小于，就是day本身
+            hour1 = hour1 < 10 ? "0" + hour1 : hour1;
+            minu1 = minu1 < 10 ? "0" + minu1 : minu1;
+            sec1 = sec1 < 10 ? "0" + sec1 : sec1;
+            if (millsec1 < 10) {
+                millsec1 = "00" + millsec1;
+            } else if (millsec < 100) {
+                millsec1 = "0" + millsec1;
+            }
+ 
+            div4.innerHTML = "距离上一次见面已经" + day1 + "天" + hour1 + "小时" + minu1 + "分" + sec1 + "秒";
+        }
